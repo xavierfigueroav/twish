f67970a5150ea0e79a86c242bacdef6e3900453c
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-04-26 04:59:56 -0500

    Create Result page to show tweets classified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Search from './pages/Search';
+import Result from './pages/Result';
 
 
 const App = () => {
     return (
         <BrowserRouter>
           <Switch>
+            <Route path="/search/:id" component={Result} />
             <Route path="/" component={Search} />
           </Switch>
         </BrowserRouter>
