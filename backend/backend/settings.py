f0334ef28353b66fbe72a5b9dc71a652212256a5
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-04-28 03:17:16 -0500

    Set up file uploading in Django and Docker
    Set up docker to share uploaded files between the Django and React apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = '/static/'
+
+MEDIA_URL = 'media/'
