96b4240ccda20449447471b79058057c751e126c
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-05-07 22:11:20 -0500

    Create SearchCard component to show search information

---
+++ frontend/src/components/SearchCard.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+import moment from 'moment';
+
+
+const SearchCard = (props) => {
+    return (
+        <Link className="bg-gray-100 rounded-sm pt-5 px-5 text-left shadow hover:shadow-md" to={props.href}>
+            <p className="mb-3 text-center">
+                {props.numberOfTweets} tweets sobre
+                <span className="font-semibold italic text-blue-900"> {props.searchTerm} </span>
+                fueron clasificados
+            </p>
+            <p className="text-right mb-2 text-gray-500 font-semibold">
+                {moment(props.date).format('MMMM Do, YYYY · HH:mm')}
+            </p>
+        </Link>
+    );
+}
+
+export default SearchCard;
