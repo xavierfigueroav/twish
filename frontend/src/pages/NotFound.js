7e8d90d3a370d0b5c6c99f03938f491807e56e07
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-05-11 19:09:22 -0500

    Translate NotFound page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
+
 const NotFound = () => {
     return (
         <div className="m-5">
             <Header />
             <main className="mt-16 mx-auto text-center md:w-3/4 lg:w-1/2">
-                <p className="text-2xl font-semibold">¡La página que estabas buscando no existe!</p>
+                <p className="text-2xl font-semibold">¡The page you were looking for does not exist!</p>
                 <p className="text-2xl font-semibold">:(</p>
                 <br />
                 <Link className="text-2xl font-semibold text-blue-900 hover:underline" to="/">
-                    Regresa al inicio
+                    Got back to home
                 </Link>
             </main>
         </div>
