9f43de8b32f3e3460824ffd6054d18d843cadca1
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-05-11 20:33:38 -0500

    Parameterize predictor labels and take them from settings.json

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,7 +1,11 @@
-export const HelpType = {
-    helpOffered: 'helpOffered',
-    helpWanted: 'helpWanted',
-};
+export const labelColors = [
+    'bg-green-400', 'bg-red-400', 'bg-blue-400', 'bg-yellow-400', 
+    'bg-pink-400', 'bg-purple-400', 'bg-gray-400',
+    'bg-green-800', 'bg-red-800', 'bg-blue-800', 'bg-yellow-800', 
+    'bg-pink-800', 'bg-purple-800', 'bg-gray-800',
+    'bg-green-100', 'bg-red-100', 'bg-blue-100', 'bg-yellow-100', 
+    'bg-pink-100', 'bg-purple-100', 'bg-gray-100'
+];
 
 export const API = {
     search: 'api/search',
