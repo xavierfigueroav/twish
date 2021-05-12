34239579b967ac43bd364dbb4882e8859d191a63
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-05-11 19:11:00 -0500

    Translate FirstSetUp page

--- frontend/src/pages/FirstSetUp.js
+++ frontend/src/pages/FirstSetUp.js
@@ -9,17 +9,17 @@ const FirstSetUp = () => {
         <main className="flex h-screen mx-10">
             <div className="m-auto md:w-1/2 bg-green-300 px-6 py-5 rounded-md border-2 border-green-500">
                 <p className="text-green-900 font-semibold text-center">
-                    No has configurado tu aplicación aún. Para hacerlo, dirígete 
-                    a <a className="underline" href={djangoAdminURL} target="blank">
+                    You have not configured your application yet. To do it, go  
+                    to <a className="underline" href={djangoAdminURL} target="blank">
                         {djangoAdminURL}
-                    </a> y crea una nueva instancia del modelo <i>App</i>.
+                    </a> and create a new instance of <i>App</i>.
                 </p>
                 <br />
                 <p className="text-green-900 font-semibold text-center">
-                    Usa las siguientes credenciales y no olvides cambiarlas en el futuro:
+                    Use the following credentials and do not forget to change it later:
                 </p>
-                <p className="text-green-900 font-semibold text-center"><i>usuario: admin</i></p>
-                <p className="text-green-900 font-semibold text-center"><i>contraseña: admin</i></p>
+                <p className="text-green-900 font-semibold text-center"><i>user: admin</i></p>
+                <p className="text-green-900 font-semibold text-center"><i>password: admin</i></p>
             </div>
         </main>
     );
